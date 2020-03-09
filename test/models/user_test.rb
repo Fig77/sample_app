b8e701fa45b300e40ae24fb8d3d645c8e4014dfa
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-03-09 16:00:33 -0300

    added password secure and updated bundle

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,24 @@ class UserTest < ActiveSupport::TestCase
   test 'create user' do
     assert @user.valid?
   end
+
+  test 'email addresses should be unique' do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test 'email addresses should be unique' do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test 'email addresses should be saved as lower-case' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
