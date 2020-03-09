e273dc7c0f5b98c2fa0650a6878ad7e78b5d9da0
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-03-09 16:34:02 -0300

    added password, gem for it, and min auth things

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password        
+  has_secure_password
+  validates :password, presence: true, length: { minimun: 6 }       
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,30 +2,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Example', email: 'example@email.com')
+    @user = User.new(name: 'Example', email: 'example@email.com', password: 'testPassw')
   end
 
   test 'create user' do
     assert @user.valid?
   end
 
-  test 'email addresses should be unique' do
-    duplicate_user = @user.dup
-    @user.save
-    assert_not duplicate_user.valid?
-  end
-
-  test 'email addresses should be unique' do
-    duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
-    @user.save
-    assert_not duplicate_user.valid?
-  end
-
-  test 'email addresses should be saved as lower-case' do
-    mixed_case_email = 'Foo@ExAMPle.CoM'
-    @user.email = mixed_case_email
-    @user.save
-    assert_equal mixed_case_email.downcase, @user.reload.email
-  end
 end
